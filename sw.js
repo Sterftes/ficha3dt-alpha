141a7abf10de998929a5db4c95da749995adf8eb
Sterftes <pablo13.pno@gmail.com>
2026-06-05 16:25:52 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-self.addEventListener('install',e=>{e.waitUntil(caches.open('ficha3dt-v91').then(c=>c.addAll(['./','./index.html','./style.css','./app.js','./manifest.json','./bestiary.js','./icon.svg'])))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => { e.respondWith(fetch(e.request)); });
